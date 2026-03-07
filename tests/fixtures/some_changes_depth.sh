789c228b4eb5be26a5ce1c281795eb5727a66ad7
Christoph Rüßler <christoph.ruessler@mailbox.org>
2026-03-07 10:34:03 +0100

    Add tests for --depth

---
+++ tests/fixtures/some_changes_depth.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+set -eu -o pipefail
+
+git init -q first
+(cd first
+  seq 1 10 >> 1.txt
+  git add 1.txt
+  git commit -q -m c1
+
+  seq 1 10 >> 2.txt
+  git add 2.txt
+  git commit -q -m c2
+
+  seq 1 10 >> 1.txt
+  seq 1 10 >> 2.txt
+)
+
+git init -q second
+(cd second
+  seq 1 10 >> 1.txt
+  git add 1.txt
+  git commit -q -m c1
+
+  seq 1 10 >> 2.txt
+  git add 2.txt
+  git commit -q -m c2
+
+  seq 1 10 >> 1.txt
+  seq 1 10 >> 2.txt
+)
