8749e64e4dc8f0b6ac99aabe4966db6d8ab389d5
Christoph Rüßler <christoph.ruessler@mailbox.org>
2026-03-08 20:10:52 +0100

    Add tests for changes in sub-directories

--- tests/fixtures/some_changes.sh
+++ tests/fixtures/some_changes.sh
@@ -11,5 +11,11 @@ seq 1 10 >> 2.txt
 git add 2.txt
 git commit -q -m c2
 
+mkdir -p a/b/c
+seq 1 10 >> a/b/c/3.txt
+git add a/b/c/3.txt
+git commit -q -m c3
+
 seq 1 10 >> 1.txt
 seq 1 10 >> 2.txt
+seq 1 10 >> a/b/c/3.txt
